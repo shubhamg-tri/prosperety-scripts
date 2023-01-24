708146b25d797daeedcc9e8d7a47d95ec23cfed1
shubhamg-tri <108567801+shubhamg-tri@users.noreply.github.com>
2023-01-24 10:17:01 +0000

    tee

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ function setCookie(cName, cValue, expDays) {
     date.setTime(date.getTime() + expDays * 24 * 60 * 60 * 1000);
     const expires = "expires=" + date.toUTCString();
     document.cookie =
-        cName + "=" + cValue + "; " + expires + "; path=/" + 'domain=myapp.example.com; SameSite=None';
+        cName + "=" + cValue + "; " + expires + "; path=/" + 'domain=myapp.example; SameSite=None';
 }
 
 // Apply setCookie
